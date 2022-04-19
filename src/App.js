9fcb54420092555a37a245de7a7b45586a242a0e
BestPizzaManNA <bestpizzamanna@gmail.com>
2022-04-19 14:11:42 -0700

    Header links made and began CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route, Link, Redirect } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<header className="page-header">
+				<Link to="/">
+					<h1 className="page-title">Michael Brock</h1>
+				</Link>
+				<div className="links-nav-container">
+					<nav className="links-nav">
+						<ul className="links">
+							<Link to="">
+								<li className="header-link">About Me</li>
+							</Link>
+							<Link to="">
+								<li className="header-link">Projects</li>
+							</Link>
+
+							<a
+								href="https://github.com/BrockPaperScissors"
+								target="_blank"
+								id="link-git"
+							>
+								<li className="header-link">Git Hub</li>
+							</a>
+
+							<Link to="">
+								<li className="header-link">Resume</li>
+							</Link>
+						</ul>
+					</nav>
+				</div>
+			</header>
+			<div>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
