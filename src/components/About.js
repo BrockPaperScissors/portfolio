871655d50ee76e0be75d6a11c76a0e4af12f80e4
BestPizzaManNA <bestpizzamanna@gmail.com>
2022-04-20 12:28:56 -0700

    Added about me life story

---
+++ src/components/About.js
@@ -0,0 +1,55 @@
+export default function About() {
+	return (
+		<div>
+			<h1>My Story:</h1>
+			<p>
+				I spent the early parts of my life growing up in the frozen tundra that
+				is Minnesota. I got into playing sports very early on, starting with
+				hockey. As soon as I could stand, I was put into a pair of skates. I
+				played that for seven or eight years before transitioning into football
+				and baseball. Around the age of 6 or 7, I was introduced to computer
+				gaming and my love of computers blossomed. Some early titles that
+				captured my imagination were Diablo 1, Warcraft II: Tides of Darkness,
+				and StarCraft. I discovered my love for MMORPGs when my older brothers
+				introduced me to Star Wars Galaxies. Around the same age, I discovered
+				extreme sports. Skateboarding, bmx biking, skiing and snowboarding,
+				anything with a little bit of danger had my name on it. When I reached
+				the age of thirteen or fourteen, my creative side began to sprout. I
+				picked up the bass guitar and began forming bands with my friends. We
+				would play every Battle of the Bands we could find. Early on in
+				highschool, I took a class in woodworking. I enjoyed that and spent two
+				years as the TA helping other students with their projects and promoting
+				safe useage of the shop tools. After finishing highschool, I picked up
+				my first job at a local movie theater. I quickly got promoted from sales
+				and concessions up to the projection booth. I would prepare the 35mm
+				film projectors for each show and ensure that the movies started on
+				time. I developed strong friendships with my co-workers and managers. A
+				group of us would occasionally get together to play rugby, eventually
+				going on to form our own team. I was co-captain for about four years,
+				during which we traveled around playing against teams from all over the
+				midwest. At the age of 21, I left the movie theater and began my time in
+				the pizza delivery business. I moved away from my home town to the Saint
+				Paul/Minneapolis area where I spent eight years being passionate about
+				the pizza industry. I even built an online persona around being a pizza
+				delivery driver! While in the Twin Cities, my creative curiosity
+				continued to develop. I spent time taking classes in blacksmithing,
+				neon-glass bending and metal casting. Still being a huge fan of PC
+				gaming, I began growing an interest in making computer games. I started
+				to dabble in some free tools such as Blender, Unity, Gimp and began
+				following tutorials I found on the internet. I didn't get too far at
+				that time, but the passion didn't fade. Fast forward to early 2021, I
+				decided I wanted to change directions and put the pizza in the fridge
+				for now. I decided make the move west, where I spent some time in Oregon
+				planning my next path. I was presented with the opportunity to move down
+				to L.A. where my older brother is currently working in the game
+				industry. I accepted his offer of guidance and mentorship towards a goal
+				of landing a job in the game industry. Staying focused on forward
+				momentum, I enrolled into a 12-week software engineering bootcamp at
+				General Assembly in January of 2022. At the time of writing this, I have
+				just completed that 12-week journey and developed the skills needed to
+				make this website! From here, the sky is the limit and I keep the
+				momentum moving forward.
+			</p>
+		</div>
+	);
+}
