d0370c60dbd59444f150ddeee286730ccba0ba26
BestPizzaManNA <bestpizzamanna@gmail.com>
2022-04-22 18:17:34 -0700

    Added personal logo to home component and welcome to page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,17 @@
-import { Link } from "react-router-dom";
+import "./css/home.css";
 export default function Home() {
-	return <div>hello</div>;
+	return (
+		<div className="home-image">
+			<img src="https://i.imgur.com/IMRMdby.png" className="pizza-pic"></img>
+			<p className="pizza-text">
+				Welcome to my portfolio site! My name is Michael Brock and I appreciate
+				you taking the time to stop by. I have recently begun my journey into
+				software development, and will use this site to reflect new skills and
+				techniques that I pick up along the way! I have a passion for both web
+				and game development, creativity, and if it isn't obvious (see image to
+				the left), pizza! Feel free to poke around, and reach out with any
+				questions, comments or suggestions.
+			</p>
+		</div>
+	);
 }
