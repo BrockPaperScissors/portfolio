aa3895bcb77be9ae4d8f170976a0e0cbe0bbdc35
BestPizzaManNA <bestpizzamanna@gmail.com>
2022-04-24 20:46:21 -0700

    modified projects page layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ export default function Projects() {
 			projectIndex: 1,
 			url: "https://book-roulette.netlify.app/",
 			previewIMG: "https://i.imgur.com/YRbIFR6.png",
-			date: "",
+			date: "April 2022",
 			about:
 				"Book Roulette is a team based project with the goal of creating a book suggestion generator. Users can select their favorite genre from a list and be recommended a random book from our database! I contributed heavily in the design and planning process of the entire project. I also contributed with the construction of several of the front-end React components, along with the design and implementation of the home page, navigation bar and the listing of books by genre.",
 		},
@@ -25,7 +25,7 @@ export default function Projects() {
 			projectIndex: 2,
 			url: "https://teamdeets.netlify.app/",
 			previewIMG: "https://i.imgur.com/UpwxVm9.png",
-			date: "",
+			date: "March 2022",
 			about:
 				"Team Deets was my second project, fueled by my love for the game League of Legends. I wanted a tool to review the details of all ten players in my recent matches. I added the functionality for other players to search their match history using their own summoner names! This was my first project utilizing third-party APIs and working with the data inside of React.js. ",
 		},
@@ -44,7 +44,7 @@ export default function Projects() {
 			projectIndex: 4,
 			url: "https://brockpaperscissors.github.io/BabyGandalph/",
 			previewIMG: "https://i.imgur.com/LmNLr4c.png",
-			date: "",
+			date: "February 2022",
 			about:
 				"Baby Gandalph was my very first project. It is a simple clicking game, based off the concept of a 'Tamagotchi' game. The user has to manage fatigue and hunger levels whilst attempting to advance the skills level.",
 		},
@@ -72,7 +72,8 @@ export default function Projects() {
 		<div className="proj-container">
 			<div className="car-container">
 				<h2 className="proj-title">
-					Project: {projArray[projectIndex].projectName}
+					Project: {projArray[projectIndex].projectName} -{" "}
+					{projArray[projectIndex].date}
 				</h2>
 				<a href={projArray[projectIndex].url}>
 					<img
