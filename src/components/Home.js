be6482edbcade810be81530856f71bb1c4ef2e48
BestPizzaManNA <bestpizzamanna@gmail.com>
2022-04-24 20:43:02 -0700

    Updated my home page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,10 @@ export default function Home() {
 		<div className="home-image">
 			<img src="https://i.imgur.com/IMRMdby.png" className="pizza-pic"></img>
 			<p className="pizza-text">
-				Welcome to my portfolio site! My name is Michael Brock and I appreciate
-				you taking the time to stop by. I have recently begun my journey into
-				software development, and will use this site to reflect new skills and
-				techniques that I pick up along the way! I have a passion for both web
-				and game development, creativity, and if it isn't obvious (see image to
-				the left), pizza! Feel free to poke around, and reach out with any
-				questions, comments or suggestions.
+				Hello! I am Michael, a recent career changer on the journey into the
+				world of software engineering. As a former pizza delivery driver, I
+				focus on the wants and needs of the people using my products. If my job
+				is done right, other people's jobs are made a little bit easier.
 			</p>
 		</div>
 	);
